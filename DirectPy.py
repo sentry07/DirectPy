10873a3f81bcd649d30c6d4fe20e3438645f3d3b
sentry07 <sentry07@gmail.com>
2014-11-12 20:58:11 -0800

    Updated module to be more Python friendly.
    Thanks Whoopska for the pointers. :)

--- DirectPy.py
+++ DirectPy.py
@@ -1,83 +1,92 @@
-#####################################################
-# DirectPy.py by Eric Walters (github.com/sentry07) #
-# ------------------------------------------------- #
-# Control a DirecTV receiver over the network using #
-# DirecTV's SHEF protocol. For more information on  #
-# the SHEF protocol, please search Google for       #
-# DirecTV SHEF 1.3                                  #
-#####################################################
+import requests
 
-import urllib.request
-import json
+class DIRECTV:
+    """DirectPy.py by Eric Walters (github.com/sentry07)
 
-class directv:
-    def __init__(self,ip,port = 8080):
+    Control a DirecTV receiver over the network using
+    DirecTV's SHEF protocol. For more information on
+    enabling the SHEF interface, please see this PDF:
+    https://www.satinstalltraining.com/homeautomation/DTV-MD-0359-DIRECTV_SHEF_Command_Set-V1.3.C.pdf
+    """    
+    def __init__(self, ip, port=8080):
         self.ip = ip
         self.port = port
         self.standby = False
         self.channel = '0'
-        
-        self.get_status()
+        self.valid_keys = ['power', 'poweron', 'poweroff', 'format', 'pause', 'rew', 'replay', 'stop',
+                          'advance', 'ffwd', 'record', 'play', 'guide', 'active', 'list', 'exit',
+                          'back', 'menu', 'info', 'up', 'down', 'left', 'right', 'select', 'red',
+                          'green', 'yellow', 'blue', 'chanup', 'chandown', 'prev', '0', '1', '2',
+                          '3', '4', '5', '6', '7', '8', '9', 'dash', 'enter']
+
+        self.base_url = 'http://%s:%s' % (ip,port)
+
+        self.get_standby()
         self.get_tuned()
 
-    ## Get status of the receiver. This only tells us if the receiver is in standby or not.
-    def get_status(self):
-        w = urllib.request.urlopen('http://%s:%d/info/mode' % (self.ip,self.port))
-        jResp = json.loads(w.read().decode('UTF-8'))
+    @staticmethod
+    def _parse_channel(channel):
+        """Return major and minor channel numbers for given channel"""
+        try:
+            major, minor = channel.split('-')
+        except ValueError:
+            major = channel
+            minor = 65535
+
+        return major,minor
+
+    @staticmethod
+    def _combine_channel(major,minor):
+        """Return the combined channel number. If minor == 65535, there is no minor channel number."""
+        if minor == 65535:
+            return major
+        else:
+            return '%s-%s' % (major,minor)
+
+    def get_standby(self):
+        """Return standby status of the receiver."""
+        jResp = requests.get('%s/info/mode' % (self.base_url)).json()
         self.standby = (jResp['mode'] == 1)
         
-    ## Return information for a channel. You can specify a standard (249) or HD (249-1) channel.
-    def get_channel(self,channel):
-        _channel = channel.split('-')
-        major = _channel[0]
-        if len(_channel) == 2:
-            minor = _channel[1]
-        else:
-            minor = 65535
-
-        w = urllib.request.urlopen('http://%s:%d/tv/getProgInfo?major=%s&minor=%s' % (self.ip,self.port,major,minor))
-        jResp = json.loads(w.read().decode('UTF-8'))
+        return self.standby
+        
+    def get_channel(self, channel):
+        """Return program information for a channel. Specify a standard (249) or HD (249-1) channel."""
+        major,minor = self._parse_channel(channel)
+        jResp = requests.get('%s/tv/getProgInfo?major=%s&minor=%s' % (self.base_url,major,minor)).json()
 
         return jResp
 
-    ## This gets the channel and program information of the current channel.
     def get_tuned(self):
-        w = urllib.request.urlopen('http://%s:%d/tv/getTuned' % (self.ip,self.port))
-        jResp = json.loads(w.read().decode('UTF-8'))
-
-        if jResp['minor'] == 65535:
-            self.channel = jResp['major']
-        else:
-            self.channel = '%s-%s' % (jResp['major'],jResp['minor'])
+        """Returns the channel and program information of the current channel."""
+        jResp = requests.get('%s/tv/getTuned' % (self.base_url)).json()
+        self.channel = self._combine_channel(jResp['major'],jResp['minor'])
         
         return jResp
 
-    ## This changes the channel on the receiver. You can specify a standard (249) or HD (249-1) channel.
-    def tune_channel(self,channel):
-        _channel = channel.split('-')
-        major = _channel[0]
-        if len(_channel) == 2:
-            minor = _channel[1]
-        else:
-            minor = 65535
+    def tune_channel(self, channel):
+        """Change the channel on the receiver. Specify a standard (249) or HD (249-1) channel."""
+        major,minor = self._parse_channel(channel)
 
-        w = urllib.request.urlopen('http://%s:%d/tv/tune?major=%s&minor=%s' % (self.ip,self.port,major,minor))
-        jResp = json.loads(w.read().decode('UTF-8'))
-        if jResp['status']['msg'] == 'OK.':
+        jResp = requests.get('%s/tv/tune?major=%s&minor=%s' % (self.base_url,major,minor)).json()
+        if jResp['status']['code'] == 200:
             self.channel = channel
 
         return jResp
 
+    def key_press(self, key):
+        """Emulate pressing a key on the remote.
 
-    ## This emulates pressing a key on the remote.
-    ## Supported keys: power, poweron, poweroff, format, 
-    ## pause, rew, replay, stop, advance, ffwd, record, 
-    ## play, guide, active, list, exit, back, menu, info, 
-    ## up, down, left, right, select, red, green, yellow, 
-    ## blue, chanup, chandown, prev, 0, 1, 2, 3, 4, 5, 
-    ## 6, 7, 8, 9, dash, enter 
-    def key_press(self,key):
-        w = urllib.request.urlopen('http://%s:%d/remote/processKey?key=%s&hold=keyPress' % (self.ip,self.port,key))
-        jResp = json.loads(w.read().decode('UTF-8'))
+        Supported keys: power, poweron, poweroff, format, 
+        pause, rew, replay, stop, advance, ffwd, record, 
+        play, guide, active, list, exit, back, menu, info, 
+        up, down, left, right, select, red, green, yellow, 
+        blue, chanup, chandown, prev, 0, 1, 2, 3, 4, 5, 
+        6, 7, 8, 9, dash, enter
+        """
+        if not key.lower() in self.valid_keys:
+            raise ValueError('Invalid key: ' + key)
+        
+        jResp = requests.get('%s/remote/processKey?key=%s&hold=keyPress' % (self.base_url,key)).json()
 
         return jResp

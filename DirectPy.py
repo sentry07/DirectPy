144b2cda0e94fdd15aec9c555e075a86e9cdf979
sentry07 <sentry07@gmail.com>
2014-11-14 23:11:38 -0800

    Added clientAddr parameter for Genie systems

--- DirectPy.py
+++ DirectPy.py
@@ -8,10 +8,17 @@ class DIRECTV:
     DirecTV's SHEF protocol. For more information on
     enabling the SHEF interface, please see this PDF:
     https://www.satinstalltraining.com/homeautomation/DTV-MD-0359-DIRECTV_SHEF_Command_Set-V1.3.C.pdf
+
+    The clientAddr parameter of the class is used for
+    Genie systems that have a server receiver and client
+    receivers. To control a client receiver, you must
+    know the MAC address and reference it without colons.
+    EX: DIRECTV('192.168.1.10',clientAddr='000A959D6816')
     """    
-    def __init__(self, ip, port=8080):
+    def __init__(self, ip, port=8080, clientAddr='0'):
         self.ip = ip
         self.port = port
+        self.clientAddr = clientAddr
         self.standby = False
         self.channel = '0'
         self.valid_keys = ['power', 'poweron', 'poweroff', 'format', 'pause', 'rew', 'replay', 'stop',
@@ -46,7 +53,7 @@ class DIRECTV:
 
     def get_standby(self):
         """Return standby status of the receiver."""
-        jResp = requests.get('%s/info/mode' % (self.base_url)).json()
+        jResp = requests.get('%s/info/mode?clientAddr=%s' % (self.base_url,self.clientAddr)).json()
         self.standby = (jResp['mode'] == 1)
         
         return self.standby
@@ -56,13 +63,13 @@ class DIRECTV:
         if not type(channel) is str:
             raise TypeError('Channel should be a string')
         major,minor = self._parse_channel(channel)
-        jResp = requests.get('%s/tv/getProgInfo?major=%s&minor=%s' % (self.base_url,major,minor)).json()
+        jResp = requests.get('%s/tv/getProgInfo?major=%s&minor=%s&clientAddr=%s' % (self.base_url,major,minor,self.clientAddr)).json()
 
         return jResp
 
     def get_tuned(self):
         """Returns the channel and program information of the current channel."""
-        jResp = requests.get('%s/tv/getTuned' % (self.base_url)).json()
+        jResp = requests.get('%s/tv/getTuned?clientAddr=%s' % (self.base_url,self.clientAddr)).json()
         self.channel = self._combine_channel(jResp['major'],jResp['minor'])
         
         return jResp
@@ -73,7 +80,7 @@ class DIRECTV:
             raise TypeError('Channel should be a string')
         major,minor = self._parse_channel(channel)
 
-        jResp = requests.get('%s/tv/tune?major=%s&minor=%s' % (self.base_url,major,minor)).json()
+        jResp = requests.get('%s/tv/tune?major=%s&minor=%s&clientAddr=%s' % (self.base_url,major,minor,self.clientAddr)).json()
         if jResp['status']['code'] == 200:
             self.channel = channel
 
@@ -94,6 +101,6 @@ class DIRECTV:
         if not key.lower() in self.valid_keys:
             raise ValueError('Invalid key: ' + key)
         
-        jResp = requests.get('%s/remote/processKey?key=%s&hold=keyPress' % (self.base_url,key)).json()
+        jResp = requests.get('%s/remote/processKey?key=%s&hold=keyPress&clientAddr=%s' % (self.base_url,key,self.clientAddr)).json()
 
         return jResp

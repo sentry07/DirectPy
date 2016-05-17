5af03c3afddc857e89529b1aa3ee2eee88b101cd
Michael Hojna <michael.hojna@gmail.com>
2016-05-17 15:55:19 -0400

    Added setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='directvpy',
+      version='0.1',
+      description='Directv Shef interface python class',
+      url='http://github.com/sentry07/DirectPy',
+      author='Eric Walters',
+      license='MIT',
+      py_modules=['DirectPy'])

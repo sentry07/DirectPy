624047599e004fa52fd8622f0bedbd73eac261b5
ehendrix23 <hendrix_erik@hotmail.com>
2018-11-29 14:49:12 -0700

    Increase version to 0.6
    Increase version to 0.6 to update pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='directpy',
-      version='0.3',
+      version='0.6',
       description='A python library for interacting with DirecTV receivers',
       url='http://github.com/sentry07/DirectPy',
       author='Eric Walters',

0a7ded08824f27d5f24b36f1cfb29c77f4302c39
sentry07 <sentry07@gmail.com>
2020-03-09 16:46:36 -0700

    Updated setup parameters
    Added author_email as is apparently now required and updated version

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
 setup(name='directpy',
-      version='0.6',
+      version='0.7',
       description='A python library for interacting with DirecTV receivers',
       url='http://github.com/sentry07/DirectPy',
       author='Eric Walters',
+      author_email='sentry07@gmail.com',
       license='MIT',
       py_modules=['DirectPy'])
